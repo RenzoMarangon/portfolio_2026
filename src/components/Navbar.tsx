4cd8f8c2e97a60b1afe4f97caf004d3018cdced6
Renzo <renzomarangon@gmail.com>
2026-03-09 23:07:50 -0300

    Navbar completo sin logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,15 @@
 import { Container } from "./Container";
-import "@radix-ui/themes/styles.css";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Links = [
   {
@@ -16,25 +26,66 @@ const Links = [
 export const Navbar = () => {
   return (
     <nav className="backdrop-blur-md shadow-sm">
-      <Container>
+      <Container className="flex h-16 items-center justify-between">
         {/* LOGO */}
-        <h1 className="">
-          <a href="/">
-            <span className="sr-only">Portfolio | Renzo Marangon</span>
+        <h1>
+          <a href="/" className="flex gap-2">
             <img src="" alt="Logo" />
+            <span className="">Portfolio | Renzo Marangon</span>
           </a>
         </h1>
 
         {/* LINKS DESKTOP*/}
-        <ul className="hidden md:flex">
+        <ul className="hidden lg:flex justify-between gap-6 text-sm ">
           {Links.map((l) => (
             <li key={l.href}>
-              <a href={l.href}>{l.label}</a>
+              <a className="hover:underline underline-offset-4" href={l.href}>
+                {l.label}
+              </a>
             </li>
           ))}
         </ul>
 
         {/* MOBILE MENU */}
+
+        <div className="flex lg:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" className="capitalize">
+                ☰
+              </Button>
+            </SheetTrigger>
+            <SheetContent
+              side="left"
+              className="data-[side=bottom]:max-h-[50vh] data-[side=top]:max-h-[50vh]">
+              <SheetHeader>
+                <SheetTitle>
+                  {/* LOGO */}
+                  <h1 className="">
+                    <a href="/">
+                      <span className="sr-only">
+                        Portfolio | Renzo Marangon
+                      </span>
+                      <img src="" alt="Logo" />
+                    </a>
+                  </h1>
+                </SheetTitle>
+              </SheetHeader>
+
+              <ul className="flex flex-col gap-2 ml-4">
+                {Links.map((l) => (
+                  <li key={l.href}>
+                    <SheetClose asChild>
+                      <a className="" href={l.href}>
+                        {l.label}
+                      </a>
+                    </SheetClose>
+                  </li>
+                ))}
+              </ul>
+            </SheetContent>
+          </Sheet>
+        </div>
       </Container>
     </nav>
   );
