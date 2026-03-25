6d73e7888d293139c240a4ebc513ac839d747418
Renzo <renzomarangon@gmail.com>
2026-03-25 01:32:14 -0300

    cambio de email de contacto

--- netlify/lib/email.ts
+++ netlify/lib/email.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function sendEmail({ nombre, asunto, mensaje }: ContactFormData) {
   return resend.emails.send({
-    from: "Portfolio <onboarding@resend.dev>",
+    from: "Portfolio <contacto@renzomarangon.com.ar>",
     to: "renzomarangon@outlook.com",
     subject: asunto,
     html: `
