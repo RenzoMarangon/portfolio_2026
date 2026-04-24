35e33a74ee50e3bbdaa8a2776a41bd31eb7f9b3a
Renzo <renzomarangon@gmail.com>
2026-04-24 20:50:48 -0300

    Video demo de DriveCar anadido

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -22,7 +22,7 @@ export const projects: Project[] = [
     title: "DriveCar App | Concesionaria de autos",
     description:
       "Aplicación web creada como proyecto final para Progamación III. Es una app de concesionaria de autos, con funcionalidades de búsqueda, filtrado y detalles de cada vehículo, conectada con SqlServer para la gestión de datos",
-    href: "https://youtube.com/",
+    href: "https://youtu.be/QCnpVQQzv7M",
     githubUrl: "https://github.com/RenzoMarangon/TP_Final_PrograIII_DriveCar",
     img: drivecar,
     skills: ["C#", "SQL Server", "ASP.NET"],
